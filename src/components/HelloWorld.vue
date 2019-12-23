d81105c6c2b97763785994640503b1595cad9c4f
Aero-bro <1498747727@qq.com>
2019-12-23 23:17:26 +0800

    初始

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <div slot="header">
+      <h1>{{msg}}</h1>
+    </div>
+
+    <Row>
+
+      <a href="https://www.baidu.com/" target="_blank">
+        <img  alt="baidu" class="baidu-png" src="@/assets/baidu.png" ></a>
+
+      <a href="https://www.y80s.net/" target="_blank">
+        <img class="80-png" src="@/assets/80s.png" style="width:400px"></a>
+
+    </Row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
